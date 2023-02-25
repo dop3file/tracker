c6aa926ce10e86feee63825f32de4b6135577b61
dop3file <49581209+dop3file@users.noreply.github.com>
2023-02-25 19:28:11 +0300

    Add frontend display artist minimal info

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,7 +1,6 @@
-function send_search_request() {
-    search_value = document.getElementById('search').value
+function sendSearchRequest(search_value) {
     if (search_value != '') {
-        fetch('http://127.0.0.1:8000/', {
+        return fetch('http://127.0.0.1:8000/', {
             mode: 'cors',
             method: "POST",
             body: JSON.stringify({
@@ -12,6 +11,40 @@ function send_search_request() {
                 'Content-Type': 'application/json'
                 },
         }).then(resp => resp.json())
-        .then(data => {console.log(data);});
+        .then(data => data);
     }
 }
+
+function display_artist_info(artist) {
+    document.getElementById('main').innerHTML = ""
+    var name = document.createElement('H1')
+    name.appendChild(document.createTextNode(artist["name"]))
+    var avatar = document.createElement('img')
+    avatar.src = artist["avatar_photo"]
+    avatar.className = "avatar"
+
+    var parent = document.getElementById('main')
+
+    parent.appendChild(avatar)
+    parent.appendChild(name)
+    
+}
+
+
+async function getArtist() {
+    search_value = document.getElementById('search').value
+    artist = await sendSearchRequest(search_value)
+    artist = JSON.parse(artist)
+    
+    localStorage.setItem('artist', JSON.stringify(artist))
+
+    display_artist_info(artist)
+
+}
+
+function is_local_values() {
+    if (localStorage.length > 0) {
+        display_artist_info(JSON.parse(localStorage.getItem('artist')))
+    }
+}
+
